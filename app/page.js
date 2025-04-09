b9c5fff6f34aa61206827da30fc26e2303e75958
Eden <edenhsia@gmail.com>
2025-04-09 19:59:04 +0800

    chore: update home menu names and reorder items

--- app/page.js
+++ app/page.js
@@ -17,28 +17,28 @@ export default function Home() {
 
         <menu className="grid grid-cols-1 gap-y-4 justify-items-center md:grid-cols-4 md:gap-x-4">
           <Link
-            href="/cards"
+            href="/intro"
             className="btn-home bg-blue border-blue-dark hover:bg-blue-dark"
+          >
+            內容介紹
+          </Link>
+          <Link
+            href="/cards"
+            className="btn-home bg-red border-red-dark hover:bg-red-dark"
           >
             買遍卡片
           </Link>
           <Link
             href="/heroes"
-            className="btn-home bg-red border-red-dark hover:bg-red-dark"
+            className="btn-home bg-yellow border-yellow-dark hover:bg-yellow-dark"
           >
             歷史英雄
           </Link>
           <Link
             href="/qa"
-            className="btn-home bg-yellow border-yellow-dark hover:bg-yellow-dark"
-          >
-            窮神問答
-          </Link>
-          <Link
-            href="/"
             className="btn-home bg-green border-green-dark hover:bg-green-dark"
           >
-            集點
+            窮神問答
           </Link>
         </menu>
       </div>
